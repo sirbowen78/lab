2207dff835fb65e4264047006a4e0f6580bb279a
Sirbowen <skullknight1337@gmail.com>
2020-09-09 20:57:22 +0800

    add documentation on script

--- file_handling/dl_file1.py
+++ file_handling/dl_file1.py
@@ -4,22 +4,47 @@ import sys
 from pathlib import Path
 import requests
 
+# This example script downloads python program for mac.
+
+# Home directory of Mac, pathlib.Path module make this easy.
 home_path = Path.home()
+# This is the sub directory under home directory.
 sub_path = "tmp"
+# The download link of python.
 url = "https://www.python.org/ftp/python/3.8.5/python-3.8.5-macosx10.9.pkg"
+
+# The header of the dl link has a Content-Length which is in bytes.
+# The bytes is in string hence has to convert to integer.
 filesize = int(requests.head(url).headers["Content-Length"])
+
+# os.path.basename returns python-3.8.5-macosx10.9.pkg,
+# without this module I will have to manually split the url by "/"
+# then get the last index with -1.
+# Example:
+# url.split("/")[-1]
 filename = os.path.basename(url)
+
+# make the sub directory, exists_ok=True will not have exception if the sub dir does not exists.
+# the dir will be created if not exists.
 os.makedirs(os.path.join(home_path, sub_path), exist_ok=True)
+
+# The absolute path to download the python program to.
 dl_path = os.path.join(home_path, sub_path, filename)
 chunk_size = 1024
+
+# Use the requests.get with stream enable, with iter_content by chunk size,
+# the contents will be written to the dl_path.
+# tqdm tracks the progress by progress.update(datasize)
 with requests.get(url, stream=True) as r:
     with open(dl_path, "wb") as f, tqdm(
-        unit="B",
-        unit_scale=True,
-        unit_divisor=1024,
-        total=filesize,
-        file=sys.stdout
+            unit="B",  # unit string to be displayed.
+            unit_scale=True,  # let tqdm to determine the scale in kilo, mega..etc.
+            unit_divisor=1024,  # is used when unit_scale is true
+            total=filesize,  # the total iteration.
+            file=sys.stdout,  # default goes to stderr, this is the display on console.
     ) as progress:
         for chunk in r.iter_content(chunk_size=chunk_size):
+            # download the file chunk by chunk
             datasize = f.write(chunk)
+            # on each chunk update the progress bar.
             progress.update(datasize)
