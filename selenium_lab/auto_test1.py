763d67a99634c3ee5fe00b6a8fedc4154c75b519
sirbowen78 <skullknight1337@gmail.com>
2020-09-13 03:34:03 +0800

    updated documentation

--- selenium_lab/auto_test1.py
+++ selenium_lab/auto_test1.py
@@ -1,5 +1,11 @@
 # Selenium practice website: http://automationpractice.com/index.php
 # Using webdriver manager module makes driver installation or update is easy, thanks to SergeyPirogov.
+# Selenium is about element interaction without human intervention, a few things learned from this exercise:
+# 1. Learn how to select value/text from dropdox box.
+# 2. Learn how to do radio button selection, if xpath does not work try css_selector, if element not found,
+# use time.sleep(2) to pause for 2 seconds before clicking on radio button.
+# 3. Anything that is expected to be visible and clickable if failed to "clicked" by selenium always try to
+# use time.sleep to pause for a short time before proceed to click.
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
@@ -26,6 +32,12 @@ class WebAppTester:
             return webdriver.Edge(executable_path=EdgeChromiumDriverManager().install())
 
     def register(self, email="test@example.com", **personal_details):
+        """
+        Method to test registeration with input data.
+        :param email: email to register
+        :param personal_details: personal details in dictionary
+        :return:
+        """
         driver = self.init_driver()
         driver.get(self.url)
         driver.find_element_by_xpath("/html/body/div/div[1]/header/div[2]/div/div/nav/div[1]/a").click()
@@ -34,6 +46,7 @@ class WebAppTester:
         email_box.send_keys(email)
         driver.find_element_by_xpath(
             '/html/body/div/div[2]/div/div[3]/div/div/div[1]/form/div/div[3]/button/span').click()
+
         # Reference radio button https://stackoverflow.com/questions/21322116/using-selenium-in-python-to-click-select-a-radio-button
         sleep(2)
         if personal_details["gender"].lower() == "mr":
@@ -44,12 +57,15 @@ class WebAppTester:
         driver.find_element_by_xpath('//*[@id="customer_lastname"]').send_keys(personal_details["lastname"])
         driver.find_element_by_xpath('//*[@id="passwd"]').send_keys(personal_details["password"])
         driver.find_element_by_css_selector(f'#days > option:nth-child({int(personal_details["day"]) + 1})').click()
+
         # Reference for calendar conversion from month name to number. https://stackoverflow.com/questions/3418050/month-name-to-month-number-and-vice-versa-in-python
         driver.find_element_by_css_selector(
             f'#months > option:nth-child({list(calendar.month_name).index(personal_details["month"].capitalize()) + 1})').click()
+
         # Reference for dropdox selection: https://intellipaat.com/community/4266/how-to-select-a-drop-down-menu-option-value-with-selenium-python
         year_dropdown = Select(driver.find_element_by_xpath('//*[@id="years"]'))
         year_dropdown.select_by_value(personal_details["year"])
+
         sleep(2)
         if personal_details["newsletter"]:
             driver.find_element_by_xpath('//*[@id="newsletter"]').click()
